cf01f8a5d7945a8b40f291446fc131fefc956093
jjueco <72325841+jjueco@users.noreply.github.com>
2023-07-15 22:33:55 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,131 +1,79 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+"""A python package for time series classification."""
 
-# Note: To use the 'upload' functionality of this file, you must:
-#   $ pipenv install twine --dev
+import pyts
+from setuptools import find_packages, setup
 
-import io
-import os
-import sys
-from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
-
-# Package meta-data.
-NAME = 'YOLOv3'
-DESCRIPTION = 'My short description for my project.'
-URL = 'https://github.com/jjueco/keras-yolo3/'
-EMAIL = 'me@example.com'
-AUTHOR = 'Awesome Soul'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
+DISTNAME = 'pyts'
+DESCRIPTION = 'A python package for time series classification'
+with open('README.md') as f:
+    LONG_DESCRIPTION = f.read()
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+MAINTAINER = 'Johann Faouzi'
+MAINTAINER_EMAIL = 'johann.faouzi@gmail.com'
+URL = 'https://github.com/johannfaouzi/pyts'
+LICENSE = 'new BSD'
+DOWNLOAD_URL = 'https://github.com/johannfaouzi/pyts'
+VERSION = pyts.__version__
+INSTALL_REQUIRES = ['numpy>=1.22.4',
+                    'scipy>=1.8.1',
+                    'scikit-learn>=1.2.0',
+                    'joblib>=1.1.1',
+                    'numba>=0.55.2']
+CLASSIFIERS = ['Development Status :: 3 - Alpha',
+               'Intended Audience :: Science/Research',
+               'Intended Audience :: Developers',
+               'License :: OSI Approved',
+               'Programming Language :: Python',
+               'Topic :: Software Development',
+               'Topic :: Scientific/Engineering',
+               'Operating System :: Microsoft :: Windows',
+               'Operating System :: POSIX',
+               'Operating System :: Unix',
+               'Operating System :: MacOS',
+               'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9',
+               'Programming Language :: Python :: 3.10',
+               'Programming Language :: Python :: 3.11']
+EXTRAS_REQUIRE = {
+    'linting': [
+        'flake8'
+    ],
+    'tests': [
+        'pytest',
+        'pytest-cov'
+    ],
+    'docs': [
+        'docutils==0.14',
+        'sphinx==1.8.5',
+        'sphinx-gallery',
+        'numpydoc',
+        'matplotlib',
+        'packaging'
+    ]
+}
+PACKAGE_DATA = {
+    'pyts': ['datasets/cached_datasets/UCR/Coffee/*.txt',
+             'datasets/cached_datasets/UCR/GunPoint/*.txt',
+             'datasets/cached_datasets/UCR/PigCVP/*.txt',
+             'datasets/cached_datasets/UEA/BasicMotions/*.arff',
+             'datasets/cached_datasets/UEA/BasicMotions/*.txt',
+             'datasets/info/*.pickle']
 }
 
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
-
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
-
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
-
-
-# Where the magic happens:
-setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
-    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
-    include_package_data=True,
-    license='MIT',
-    classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
-    ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
-)
+setup(name=DISTNAME,
+      maintainer=MAINTAINER,
+      maintainer_email=MAINTAINER_EMAIL,
+      description=DESCRIPTION,
+      license=LICENSE,
+      url=URL,
+      version=VERSION,
+      download_url=DOWNLOAD_URL,
+      long_description=LONG_DESCRIPTION,
+      long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+      zip_safe=False,
+      classifiers=CLASSIFIERS,
+      packages=find_packages(),
+      package_data=PACKAGE_DATA,
+      install_requires=INSTALL_REQUIRES,
+      extras_require=EXTRAS_REQUIRE)
